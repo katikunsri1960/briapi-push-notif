298413e2be0be79a4665d690ab197a21f72b378f
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-11-20 12:05:20 +0700

    init

---
+++ database/migrations/2024_11_20_045846_create_clients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_id', 30)->unique();
+            $table->string('client_secret');
+            $table->string('partner_id');
+            $table->text('rsa_public_key');
+            $table->string('ip_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
